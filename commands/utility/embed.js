3f52ceb51dca310fb210b39893e4ebbbb5ff3e2a
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-02-14 17:51:28 -0500

    Issue #6: added subcommands and interaction options for all 3 sites

--- commands/utility/embed.js
+++ commands/utility/embed.js
@@ -4,18 +4,58 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("embed")
     .setDescription("embeds the link given")
-    .addStringOption((option) =>
-      option
-        .setName("url")
-        .setDescription("url link posted")
-        .setRequired(true)
-        .addChoices(
-          { name: "Twitter", value: "url_twitter" },
-          { name: "Instagram", value: "url_instagram" },
-          { name: "Tiktok", value: "url_tiktok" }
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("twitter")
+        .setDescription("twitter site")
+        .addStringOption((option) =>
+          option.setName("url").setDescription("twitter url")
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("instagram")
+        .setDescription("instagram site")
+        .addStringOption((option) =>
+          option.setName("url").setDescription("instagram url")
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("tiktok")
+        .setDescription("tiktok url")
+        .addStringOption((option) =>
+          option.setName("url").setDescription("tiktok url")
         )
     ),
+  //
+  //
+  // .addChoices(
+  //   { name: "Twitter", value: "url_twitter" },
+  //   { name: "Instagram", value: "url_instagram" },
+  //   { name: "Tiktok", value: "url_tiktok" }
+  // )
+  // .addStringOption((option) =>
+  //   option.setName("instagram").setDescription("instagram link posted")
+  // )
+  // .addStringOption((option) =>
+  //   option.setName("tiktok").setDescription("tiktok link posted")
+  // ),
+
   async execute(interaction) {
-    const url = interaction.options.getString("url");
+    if (interaction.options.getSubcommand() === "twitter") {
+      const twitter = interaction.options.getString("url");
+
+      if (twitter) {
+        await interaction.reply(`https://fixup{url.params}`);
+      }
+    } else if (interaction.options.getSubcommand() === "instagram") {
+      const instagram = interaction.options.getString("url");
+      if (instagram) {
+        await interaction.reply(`https://dd{url.params}`);
+      }
+    } else {
+      await interaction.reply(`https://vx{url.params}`);
+    }
   },
 };
