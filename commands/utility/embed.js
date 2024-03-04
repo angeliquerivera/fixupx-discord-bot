9769f543b19f97598f7d24f3d42fb947ff52332e
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-03-04 17:39:11 -0500

    Issue #8: add embed constructor

--- commands/utility/embed.js
+++ commands/utility/embed.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
@@ -28,6 +29,27 @@ module.exports = {
           option.setName("url").setDescription("tiktok url")
         )
     ),
+    
+    const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('Some title')
+    .setURL('https://discord.js.org/')
+    .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    .setDescription('Some description here')
+    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    .addFields(
+      { name: 'Regular field title', value: 'Some value here' },
+      { name: '\u200B', value: '\u200B' },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' }),
+  
+  channel.send({ embeds: [exampleEmbed] }),
+    
   //
   //
   // .addChoices(
